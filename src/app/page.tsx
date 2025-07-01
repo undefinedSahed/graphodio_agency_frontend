8397f0ed0ddf3f7ec72a2f3aaad82b019d7edb3f
sirsahed <sahedrahmanshakil@gmail.com>
2025-07-02 01:43:21 +0600

    works of home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import AboutSection from "@/components/home/abuout-section";
 import Banner from "@/components/home/banner";
+import HomeWorks from "@/components/home/home-works";
 import HowWeWork from "@/components/home/how-we-work";
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
       <Banner />
       <HowWeWork />
       <AboutSection />
+      <HomeWorks />
     </main>
   );
 }
