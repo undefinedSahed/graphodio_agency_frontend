c0a705088cf11826bd0ace9fe12398d9b9dcb419
lamisadiya <lamisa.diya09@gmail.com>
2025-07-08 17:52:19 +0600

    updated portfolio

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,9 +1,12 @@
 import Portfolio from "@/components/portfolio/portfolio"
 import Hero from "@/components/portfolio/hero"
+import LogoCarousel from "@/components/portfolio/LogoCarousel"
 import Footer from "@/components/home/Footer"
 export default function PortfolioPage(){
     return(
-    <><Hero /><Portfolio /><Footer/></>
+    <><Hero />
+    <LogoCarousel/>
+    <Portfolio /><Footer/></>
 
 )
 }
