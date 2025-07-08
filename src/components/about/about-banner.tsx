e52e893ec72e24424184ba1dacf0d147f8e8d226
sirsahed <sahedrahmanshakil@gmail.com>
2025-07-08 23:59:43 +0600

    refactor: enhance animation effects and improve type safety in AboutBanner component

--- src/components/about/about-banner.tsx
+++ src/components/about/about-banner.tsx
@@ -3,34 +3,54 @@
 import React, { useRef } from 'react'
 import Image from 'next/image'
 import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 
 
 export default function AboutBanner() {
 
-    const aboutRef = useRef(null)
+    const aboutRef = useRef<HTMLElement | null>(null)
 
     useGSAP(() => {
 
         const tl = gsap.timeline();
 
-        tl.fromTo(aboutRef.current, {
-            opacity: 0,
-            y: 100
-        }, {
-            opacity: 1,
-            y: 0,
-            duration: 1
-        });
+        tl.fromTo(aboutRef.current,
+            {
+                opacity: 0,
+                y: 200,
+                duration: 1,
+                ease: "power2.out",
+                visibility: "invisible"
+            },
+            {
+                opacity: 1,
+                y: 0,
+                visibility: "visible",
+                ease: "power2.out",
+                duration: 1
+            });
 
+
+        if (aboutRef.current) {
+            tl.from(aboutRef.current.querySelector('.text_start'),
+                {
+                    x: "40%",
+                    opacity: 0.6,
+                    y: -150,
+                    duration: 1,
+                    ease: "power2.out",
+                    visibility: "invisible"
+                });
+        }
     }, { scope: aboutRef })
 
 
     return (
-        <section className="min-h-screen flex place-items-end pb-20 justify-center text-white bg-[url('/images/map.png')] bg-cover bg-center z-10 absolute top-0 w-full" ref={aboutRef}>
+        <section className="min-h-screen flex place-items-end pb-20 justify-center text-white bg-[url('/images/map.png')] bg-cover bg-center z-10 absolute top-0 w-full invisible" ref={aboutRef}>
             <div className="absolute inset-0 w-full h-full bg-black opacity-50 -z-10"></div>
             <div className="container">
                 <div className="flex justify-between items-end">
-                    <div className="max-w-xl">
+                    <div className="max-w-xl text_start">
                         <p className="text-lg lg:text-6xl font-bold text-gray-300">
                             I&apos;m Eliot, Freelance Webdesigner & Webflow developer Based in Lyon, France
                         </p>
