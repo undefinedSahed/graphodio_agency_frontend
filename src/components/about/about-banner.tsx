39d624f0fb70ded37e34fa7f8bfc3007f43620ae
sirsahed <sahedrahmanshakil@gmail.com>
2025-07-25 02:24:30 +0600

    about page banner updated

--- src/components/about/about-banner.tsx
+++ src/components/about/about-banner.tsx
@@ -59,10 +59,10 @@ export default function AboutBanner() {
         <section className="lg:min-h-screen flex lg:place-items-end place-items-center pb-20 justify-center text-white bg-[url('/images/map.png')] relative z-10 bg-cover bg-center invisible lg:-mt-20 py-20 lg:py-0" ref={aboutRef}>
             <div className="absolute inset-0 w-full h-full bg-black opacity-50"></div>
             <div className="container">
-                <div className="flex justify-between items-end">
-                    <div className="max-w-xl text_start">
-                        <p className="text-lg lg:text-6xl font-bold text-gray-300">
-                            GRAPHODIO is a full-service creative agency delivering impactful solutions in design, branding, web development, content, and beyond. 
+                <div className="flex flex-col lg:flex-row justify-between lg:items-end items-center">
+                    <div className="max-w-xl text_start lg:order-1 order-2">
+                        <p className="text-lg lg:text-5xl lg:pb-12 font-bold text-gray-300 text-center lg:text-start">
+                            GRAPHODIO is a full-service creative agency delivering impactful solutions in design, branding, web development, content, and beyond.
                         </p>
                     </div>
                     <div className="max-w-md text-end animate_logo lg:order-2 order-1">
