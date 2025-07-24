8f19997887ee216b013e7ecece4eb4c7c4b33814
lamisadiya <lamisa.diya09@gmail.com>
2025-07-24 16:28:12 +0600

    content updated

--- src/components/home/banner.tsx
+++ src/components/home/banner.tsx
@@ -81,11 +81,11 @@ export default function Banner() {
             <section className="py-10 text-white/90 flex justify-center" ref={bannerRef}>
                 <div className="container">
                     <div className="text-center max-w-4xl mx-auto">
-                        <h1 className="headline invisible text-[72px] font-bold leading-tight text-center pb-4">
-                            Crafting Bold & Memorable Websites
+                        <h1 className="headline invisible text-[60px] font-bold leading-tight text-center pb-4">
+                            Crafting Visuals That Make Your Brand Stand Out
                         </h1>
-                        <p className="para invisible px-4 md:px-64 text-xl">
-                            I&apos;m Eliot, Freelance Webdesigner & Webflow developer since 2018. Based in Lyon, France
+                        <p className="para invisible px-4 md:px-64 text-base">
+                            At Graphodio, we are a full-service creative agency driven by innovation, passion, and a commitment to delivering exceptional design solutions. Since our inception, we’ve been transforming ideas into stunning visuals, building brands that resonate, and creating experiences that leave a lasting impact.
                         </p>
                     </div>
                 </div>
